b12e5c36f1ba0c73f1e95958ca09ecaa32519df3
syed-tw <60807649+syed-tw@users.noreply.github.com>
2022-08-07 18:24:14 +0530

    Post to Firebase

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,12 +1,25 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
-function NewMeetupsPage() {
+function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-b06f1-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+  }
+
   return (
     <section>
-      <h1>Add New Meetup </h1>
-      <NewMeetupForm />
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
 
-export default NewMeetupsPage;
+export default NewMeetupPage;
