95cafbf4b397bc03bb55f17aada93c3147857cea
syed-tw <60807649+syed-tw@users.noreply.github.com>
2022-08-09 19:10:05 +0530

    Added Logic to Favourites

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,7 +1,12 @@
-import { Link } from 'react-router-dom';
-import classes from './MainNavigation.module.css';
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+
+import classes from "./MainNavigation.module.css";
+import FavoritesContext from "../../store/favorites-context";
 
 function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext);
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
@@ -11,10 +16,15 @@ function MainNavigation() {
             <Link to="/">All Meetups</Link>
           </li>
           <li>
-            <Link to="/new-meetup">Add New Meetups</Link>
+            <Link to="/new-meetup">Add New Meetup</Link>
           </li>
           <li>
-            <Link to="/favourites">My Favourites</Link>
+            <Link to="/favorites">
+              My Favorites
+              <span className={classes.badge}>
+                {favoritesCtx.totalFavorites}
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
